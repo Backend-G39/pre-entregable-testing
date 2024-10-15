f35344786d50b2211a208ddfbf35350a9482377a
Gabriel <martinezgabrielalejandro4@gmail.com>
2024-10-14 22:44:14 -0300

    advances

---
+++ src/services/student.services.js
@@ -0,0 +1,35 @@
+const Student = require("../models/Student");
+
+const getAllStudents = async () => {
+  return await Student.findAll();
+}
+
+const createStudents = async (body) => {
+  return await Student.create(body);
+}
+
+const getOneStudents = async (id) => {
+  return await Student.findByPk(id);
+}
+
+const updateStudents = async (body, id) => {
+  return await Student.update(
+    body,
+    { where: { id }, returning: true }
+  );
+}
+
+const removeStudents = async (id) => {
+  return await Student.destroy({ where: { id } });
+}
+
+
+
+module.exports = {
+  getAllStudents,
+  createStudents,
+  getOneStudents,
+  updateStudents,
+  removeStudents
+
+}
