67d5f80b0d1a1f93447eac3abfd4a17f84d9f335
Gabriel <martinezgabrielalejandro4@gmail.com>
2024-10-15 00:43:16 -0300

    advances

--- src/tests/students.test.js
+++ src/tests/students.test.js
@@ -1,7 +1,7 @@
 const app = require('../app')
 const request = require('supertest')
-
 const BASE_URL = '/api/v1/students'
+
 
 const students = {
   firstName: 'Ivan',
@@ -11,6 +11,10 @@ const students = {
 }
 
 let studentId
+
+const studentUpdate = {
+  firstName: 'Juan',
+}
 
 //! test del post
 test("POST -> 'BASE_URL',should return status code 201, and res.body to be defined, res.body.firstName === students.firstName", async () => {
@@ -50,7 +54,26 @@ test("GET -> 'BASE_URL/:id' should return status code 200, and res.body.firstNam
 });
 
 
-//! test del put
+// //! test del update
+test("UPDATE -> 'BASE_URL/:id', should return status code 200 and res.body.firstName to be studentUpdate.firstName ", async () => {
+  const res = await request(app)
+    .put(`${BASE_URL}/${studentId}`)
+    .send(studentUpdate)
+
+  expect(res.status).toBe(200)
+  expect(res.body).toBeDefined()
+  expect(res.body.firstName).toBe(studentUpdate.firstName)
+  expect(res.body.id).toBe(studentId)
+})
 
 
 //! test del remove
+
+test("Delete -> 'BASE_URL/:id', should return status code 204", async () => {
+  const res = await request(app)
+    .delete(`${BASE_URL}/${studentId}`)
+
+  expect(res.statusCode).toBe(204)
+})
+
+
